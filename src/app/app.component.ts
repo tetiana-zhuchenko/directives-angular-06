2ffaf2931f72aa1f20bd922888b89d989d02632f
Tania Zhuchenko <taniyhamail@gmail.com>
2024-08-27 11:38:59 +0300

    feat: added host "directives" and composition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthDirective } from './auth/auth.directive';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
+  imports: [
+    AuthComponent,
+    LearningResourcesComponent,
+    AuthDirective,
+    LogDirective,
+  ],
 })
 export class AppComponent {}
